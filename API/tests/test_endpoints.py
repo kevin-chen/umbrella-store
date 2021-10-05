1e32c5b286ac53bd24a90a89aaee769434ae8a50
kevin-chen <kc3585@nyu.edu>
2021-10-05 16:20:28 +0000

    add test for endpoint

--- API/tests/test_endpoints.py
+++ API/tests/test_endpoints.py
@@ -1,6 +1,6 @@
 from unittest import TestCase, skip
-
 import API.endpoints as ep
+from flask_restx import Resource, Api
 
 class EndpointTestCase(TestCase):
     def setUp(self):
@@ -10,4 +10,6 @@ class EndpointTestCase(TestCase):
         pass
 
     def test_hello(self):
-        self.assertTrue(False)
+        hello = ep.HelloWorld(Resource)
+        ret = hello.get()
+        self.assertIsInstance(ret, dict)
